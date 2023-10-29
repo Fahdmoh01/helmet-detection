df6a833a1e6b837fbcf7de911b80fab7d34f238c
Fahd Mohammed <fahdmoh.1@gmail.com>
2023-10-29 22:03:47 +0530

    pushed the model to s3 bucket

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-import os
+from fastapi import FastAPI, File
+from uvicorn import run as app_run
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import Response, JSONResponse
+from helmet.constants import APP_HOST, APP_PORT
 from helmet.pipeline.train_pipeline import TrainPipeline
+from helmet.pipeline.prediction_pipeline import PredictionPipeline
 
-train_pipeline = TrainPipeline()
-train_pipeline.run_pipeline()
-print("Sucess")
-# print(os.environ)
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@pp.get("/train")
+async def training():
+    try:
+        train_pipeleine = TrainPipeline()
+        train_pipeleine.run_pipeline()
+
+        return Response("Training successful !!")
+    except Exception as e:
+        return Response(f"Error Occurred!{e}")
