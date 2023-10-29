4d13382d749529c214f40f8595ae0af411af5518
Fahd Mohammed <fahdmoh.1@gmail.com>
2023-10-29 13:24:55 +0530

    data transformation added

--- src/helmet/entity/artifacts_entity.py
+++ src/helmet/entity/artifacts_entity.py
@@ -6,3 +6,27 @@ class DataIngestionArtifacts:
     train_file_path: str
     test_file_path: str
     valid_file_path: str
+
+
+@dataclass
+class DataTransformationArtifacts:
+    transformed_train_object: str
+    transformed_test_object: str
+    number_of_classes: int
+
+
+@dataclass
+class ModelTrainerArtifacts:
+    trained_model_path: str
+
+
+@dataclass
+class ModelEvaluationArtifacts:
+    is_model_accepted: bool
+    all_losses: str
+
+
+@dataclass
+class ModelPusherArtifacts:
+    bucket_name: str
+    s3_model_path: str
